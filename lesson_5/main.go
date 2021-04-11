0a6fe1ede5325d3e8776f6278b3168bafca7a331
fesenko_darya <darya.dobrafesenko@mail.ru>
2021-04-12 00:14:39 +0300

    Первые 2 задания

---
+++ lesson_5/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	fib "./fibonacci"
+	"fmt"
+)
+
+func main()  {
+
+	// Первое и Второе задание с кэшом из мапы
+	fib.Fibonacci(16)
+	fib.Fibonacci(4)
+	fib.Fibonacci(16)
+	fib.Fibonacci(4)
+
+
+}
+

7a5803245d481cecc99b7c278eddd49b3711498d
daryafesenko <darya.dobrafesenko@mail.ru>
2021-04-12 16:46:38 +0300

    Добавила 3 и 4 задание

--- lesson_5/main.go
+++ lesson_5/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	fib "./fibonacci"
+	calc "./calculator"
 	"fmt"
 )
 
@@ -13,6 +14,28 @@ func main()  {
 	fib.Fibonacci(16)
 	fib.Fibonacci(4)
 
+	// Задания 3 и 4
+	var num1, num2 float32
+	var operation string
+
+	fmt.Printf("Программа калькулятор\nПоддерживаются такие действия, как :\n   +    -    *   / \n")
+
+	fmt.Print("Введите первое число : ")
+	fmt.Scan(&num1)
+
+	fmt.Print("Введите второе число : ")
+	fmt.Scan(&num2)
+
+	fmt.Print("Введите действие : ")
+	fmt.Scan(&operation)
+
+	res, err := calc.Calculator(num1, num2, operation)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res)
+	}
 
 }
 
